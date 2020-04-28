619b1e6238799cee749e06dcdffe34bf78a5db79
KalyanPaladugu <kalyanchakravarthi.1994@gmail.com>
2020-04-28 18:20:59 +0530

    Updates

---
+++ precache-manifest.1c9256052883d0e1e318cc208f2fb11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276e5d37008a244e96cac2db619789b0",
+    "url": "/kalayan-react/index.html"
+  },
+  {
+    "revision": "7244ecf7a714f6b1d843",
+    "url": "/kalayan-react/static/css/main.224107a3.chunk.css"
+  },
+  {
+    "revision": "f48ec87afb19bef248c8",
+    "url": "/kalayan-react/static/js/2.5d4f470c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kalayan-react/static/js/2.5d4f470c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7244ecf7a714f6b1d843",
+    "url": "/kalayan-react/static/js/main.28403e5d.chunk.js"
+  },
+  {
+    "revision": "c71754b81a1232168502",
+    "url": "/kalayan-react/static/js/runtime-main.6dcdbf48.js"
+  }
+]);
